d8c142b5a9085903cc2f438c7e464c4c560fbe39
Amin <solamin.amin@gmail.com>
2019-04-16 11:18:07 -0700

    changing directory

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import urllib.request
 from datetime import date
 from os import path
@@ -9,12 +10,11 @@ with urllib.request.urlopen('https://hangouts.google.com/') as googleHangoutPage
     match = bgFinderPattern.search(str(googleHangoutPage.read()))
     if match:
         print(match.group(1))
-        newbg = 'images/' + str(date.today()) + match.group(2)
-        result = urllib.request.urlretrieve(match.group(1), 'images/' + str(date.today()) + match.group(2))
+        newBackgroundFile = path.expanduser('~') + '/Pictures/Google-Wallpapers/' + str(date.today()) + match.group(2)
+        result = urllib.request.urlretrieve(match.group(1), newBackgroundFile)
         if result[0]:
-            imagePath = path.abspath(newbg)
-            system("gsettings set org.gnome.desktop.background picture-uri " + imagePath)
-
-
+            imagePath = path.abspath(newBackgroundFile)
+            cmd = "gsettings set org.gnome.desktop.background picture-uri " + imagePath
+            system(cmd)
     else:
         print("NO MATCH!!!")
